b13cd65b9d9d5cfa0e8ff48e6b1b8d796ceb7f70
@derevninaa <a.a.derevnin@gmail.com>
2020-07-16 12:22:46 +0300

    Initial Commit

---
+++ springbootdiplom/src/main/java/ru/derevnin/springbootdiplom/SpringbootdiplomApplication.java
@@ -0,0 +1,13 @@
+package ru.derevnin.springbootdiplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdiplomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdiplomApplication.class, args);
+	}
+
+}
---
+++ springbootdiplom/src/test/java/ru/derevnin/springbootdiplom/SpringbootdiplomApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.derevnin.springbootdiplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdiplomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
